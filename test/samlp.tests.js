daea7eea12833120d73485dca5098495a55a6325
Hernán Tierno <htierno@gmail.com>
2016-12-14 14:26:11 -0300

    Add samlp-logout module. Refactor logout logic to use it

--- test/samlp.tests.js
+++ test/samlp.tests.js
@@ -123,6 +123,46 @@ describe('samlp', function () {
       expect(xmlhelper.getAuthnContextClassRef(signedAssertion).textContent)
         .to.equal('urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified');
     });
+
+    describe('logout SP initiated - Redirect binding (GET)', function () {
+      var logoutResultValue;
+
+      // SAMLRequest: base64 encoded + deflated + URLEncoded
+      // Signature: URLEncoded
+      // SigAlg: URLEncoded
+
+      // <samlp:LogoutRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="samlr-220c705e-c15e-11e6-98a4-ecf4bbce4318" IssueInstant="2016-12-13T18:01:12Z" Version="2.0">
+      //   <saml:Issuer>https://foobarsupport.zendesk.com</saml:Issuer>
+      //   <saml:NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress">foo@example.com</saml:NameID>
+      //   <saml:SessionIndex>1</saml:SessionIndex>
+      // </samlp:LogoutRequest>
+      before(function (done) {
+        request.get({
+          jar: request.jar(),
+          followRedirect: false,
+          uri: 'http://localhost:5050/logout?SAMLRequest=fZFBS8NAEIXvhf6HsPdNM2mtdWiDhSIEqgdbPHjbbqYazO7GnQ0Uf73bVDAKetnDm%2B%2FNm8cuWZmmxa17cV14pPeOOCQn01jGfrISnbfoFNeMVhliDBp36%2Fst5mmGrXfBadeIgeV%2Fh2ImH2pnRVJuVuJs8DLPM32dXZHUEB8AmsubhZpJ0sfZ4aBpNoVF5Jk7Ki0HZcNK5BnMJeQSpntYYAYI%2BbNInshzXB7HaSaK8ShJlucI7L2%2BeA2hZZxMjs4dlOeubZ0P6QfZivgt1c4sJ0P82%2F8Qi5Sb5M55o8LfDSGFXqkreexRJKPqZl1VnphFEXNv6aRM29Ag7bJ8kLaLcGxRxrNOBXxRP8Tx6KL%2B%2BrniEw%3D%3D&Signature=KH%2FBMO0DJyS2Ffy%2B6Rnb11pAF37Y%2Beua7RHcFhVrwgxJEqsx59vTelrfPt771JPfr7%2BoG1uYwwO3Algs59yTeqmU35x18Bf2e0yWugqEF7wxHETCjrwbCK1YjYg0ilwCojk%2FBTTv2Rs%2BY7RB21Ou1GShT1uXv8WItj7E2qnr%2B6kHY5XJWTJukZa9Vnkx%2FiisA7n6UfnnGcWMdltYeOvyHvOFMVG43dDxBms9WKMKdxn6NJ7i2V1v7nXj1DoXD4PDH5B6aevkA49c6mpzozyXKLeXLys%2FvPNNT4cC1jmWvuen5pe%2FE1WfgcZcZvj2GGaxs36fdH%2FHsIcyDvE%2Bj7ngYw%3D%3D&RelayState=123&SigAlg=http%3A%2F%2Fwww.w3.org%2F2000%2F09%2Fxmldsig%23rsa-sha1'
+        }, function (err, response, b){
+          if(err) return done(err);
+          expect(response.statusCode).to.equal(302);
+          var qs = require('querystring');
+          var i = response.headers.location.indexOf('SAMLResponse=');
+          var SAMLResponse = qs.parse(response.headers.location.substr(i)).SAMLResponse;
+          
+          zlib.inflateRaw(new Buffer(SAMLResponse, 'base64'), function (err, decodedAndInflated) {
+            if(err) return done(err);
+            signedAssertion = /(<samlp:StatusCode.*\/>)/.exec(decodedAndInflated)[1];
+            var doc = new xmldom.DOMParser().parseFromString(signedAssertion);
+            logoutResultValue = doc.documentElement.getAttribute('Value');
+
+            done();
+          });
+        });
+      });
+
+      it('should respond with a Success value', function () {
+        expect(logoutResultValue).to.equal('urn:oasis:names:tc:SAML:2.0:status:Success');
+      });
+    });
   });
 
   describe('SAMLRequest on querystring with a specific authnContextClassRef', function () {
