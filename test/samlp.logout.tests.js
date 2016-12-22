a643e5c073af4046c91aa5e217d21c12adb7175d
Marcos Castany <marcos.castany@auth0.com>
2016-12-22 16:28:56 +0100

    Fix TODO: default should be post for logout

--- test/samlp.logout.tests.js
+++ test/samlp.logout.tests.js
@@ -196,4 +196,43 @@ describe('samlp logout', function () {
       expect(body).to.equal('OK');
     });
   });
+
+  describe.only('SP initiated - with default configuration', function(){
+    before(function(){
+      // Remove it 
+      server.options.protocolBinding = '';
+    });
+
+    // SAMLRequest: base64 encoded + deflated + URLEncoded
+    // Signature: URLEncoded
+    // SigAlg: URLEncoded
+
+    // <samlp:LogoutRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="samlr-220c705e-c15e-11e6-98a4-ecf4bbce4318" IssueInstant="2016-12-13T18:01:12Z" Version="2.0">
+    //   <saml:Issuer>https://foobarsupport.zendesk.com</saml:Issuer>
+    //   <saml:NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress">foo@example.com</saml:NameID>
+    //   <saml:SessionIndex>1</saml:SessionIndex>
+    // </samlp:LogoutRequest>
+    before(function (done) {
+      request.get({
+        jar: request.jar(),
+        followRedirect: false,
+        uri: 'http://localhost:5050/logout?SAMLRequest=fZFBS8NAEIXvhf6HsPdNM2mtdWiDhSIEqgdbPHjbbqYazO7GnQ0Uf73bVDAKetnDm%2B%2FNm8cuWZmmxa17cV14pPeOOCQn01jGfrISnbfoFNeMVhliDBp36%2Fst5mmGrXfBadeIgeV%2Fh2ImH2pnRVJuVuJs8DLPM32dXZHUEB8AmsubhZpJ0sfZ4aBpNoVF5Jk7Ki0HZcNK5BnMJeQSpntYYAYI%2BbNInshzXB7HaSaK8ShJlucI7L2%2BeA2hZZxMjs4dlOeubZ0P6QfZivgt1c4sJ0P82%2F8Qi5Sb5M55o8LfDSGFXqkreexRJKPqZl1VnphFEXNv6aRM29Ag7bJ8kLaLcGxRxrNOBXxRP8Tx6KL%2B%2BrniEw%3D%3D&Signature=KH%2FBMO0DJyS2Ffy%2B6Rnb11pAF37Y%2Beua7RHcFhVrwgxJEqsx59vTelrfPt771JPfr7%2BoG1uYwwO3Algs59yTeqmU35x18Bf2e0yWugqEF7wxHETCjrwbCK1YjYg0ilwCojk%2FBTTv2Rs%2BY7RB21Ou1GShT1uXv8WItj7E2qnr%2B6kHY5XJWTJukZa9Vnkx%2FiisA7n6UfnnGcWMdltYeOvyHvOFMVG43dDxBms9WKMKdxn6NJ7i2V1v7nXj1DoXD4PDH5B6aevkA49c6mpzozyXKLeXLys%2FvPNNT4cC1jmWvuen5pe%2FE1WfgcZcZvj2GGaxs36fdH%2FHsIcyDvE%2Bj7ngYw%3D%3D&RelayState=123&SigAlg=http%3A%2F%2Fwww.w3.org%2F2000%2F09%2Fxmldsig%23rsa-sha1'
+      }, function (err, response){
+        if(err) return done(err);
+        expect(response.statusCode).to.equal(200);
+        $ = cheerio.load(response.body);
+        var SAMLResponse = $('input[name="SAMLResponse"]').attr('value');
+        var samlResponse = new Buffer(SAMLResponse, 'base64');
+        signedAssertion = /(<samlp:StatusCode.*\/>)/.exec(samlResponse)[1];
+        var doc = new xmldom.DOMParser().parseFromString(signedAssertion);
+        logoutResultValue = doc.documentElement.getAttribute('Value');
+
+        done();
+      });
+    });
+
+    it('should respond with a Success value', function () {
+      expect(logoutResultValue).to.equal('urn:oasis:names:tc:SAML:2.0:status:Success');
+    });
+  });
 });
