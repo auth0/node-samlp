7b92a7521ebda4c782b9345066870d81473360ae
Luis Miranda <161006+luuuis@users.noreply.github.com>
2019-09-12 16:59:32 +0100

    removes unused utils.getRoundTripDateFormat()

--- test/utils.tests.js
+++ test/utils.tests.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const utils = require('../lib/utils');
 
 describe('utils', function () {
-  describe('getRoundTripDateFormat', function () {
+  describe('generateInstant', function () {
     it('should pad the millis appropriately', function () {
       timekeeper.withFreeze(0, () => {
         expect(utils.generateInstant()).to.equal('1970-01-01T00:00:00.000Z');
